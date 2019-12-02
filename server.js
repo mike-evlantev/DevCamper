10d105fd02837d90bb70085cb10f7eb34f1a8db3
Mike Evlantev <mikeev21@gmail.com>
2019-12-01 17:52:24 -0800

    first attempt at customer middleware - rudimentary logger

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
-// Routes
+const logger = require("./middleware/logger");
 const bootcamps = require("./routes/bootcamps");
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+// Middleware
+app.use(logger);
+
+// Routes
 app.use("/api/v1/bootcamps", bootcamps);
 
+// Server
 app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
