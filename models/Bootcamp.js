75b73987f006bde2b37044d58281c9747b68b55e
Mike Evlantev <mikeev21@gmail.com>
2019-12-08 17:06:14 -0800

    added slugify package and mongoose hook to schema

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const BootcampSchema = new mongoose.Schema(
   {
@@ -110,10 +111,10 @@ const BootcampSchema = new mongoose.Schema(
 );
 
 // Create bootcamp slug from the name
-// BootcampSchema.pre("save", function(next) {
-//   this.slug = slugify(this.name, { lower: true });
-//   next();
-// });
+BootcampSchema.pre("save", function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
 
 // Geocode & create location field
 // BootcampSchema.pre("save", async function(next) {
