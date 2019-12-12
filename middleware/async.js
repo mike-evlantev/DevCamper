5209ed13a5a083dab979a390b9ce5d9b4029546c
Mike Evlantev <mikeev21@gmail.com>
2019-12-11 21:32:32 -0800

    improved documentation

--- middleware/async.js
+++ middleware/async.js
@@ -2,10 +2,18 @@
 
 // The asyncHandler receives a function and returns a function with three input params. This new function is responsible to executes the original function passing the three params and catching any error.
 
-// Example:
-// app.get('/hello', asyncHandler( (req, res, next) => {
-//   // Some code here. Any error will be catch and pass to expressjs
-//   // No need in a try/catch
+// BEFORE:
+// app.get('/hello', async (req, res, next) => {
+//    try {
+//      // Some code here.
+//    } catch (error) {
+//      // Some error handling here.
+//    }
+// });
+
+// AFTER:
+// app.get('/hello', asyncHandler(async (req, res, next) => {
+//   // Some code here. Any error will be cought and passed to expressjs
 // }) );
 
 const asyncHandler = fn => (req, res, next) =>
