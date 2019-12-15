7243f69b7a866e4d7eb2586d7cf8572e645bb96e
Mike Evlantev <mikeev21@gmail.com>
2019-12-14 21:13:42 -0800

    register user: save to db and encrypt password (user schema middleware)

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,5 +7,13 @@ const asyncHandler = require("../middleware/async");
 // @desc    Register a user
 // @access  Public
 exports.registerAsync = asyncHandler(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
+  // Create User
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role
+  });
   res.status(200).json({ success: true });
 });
