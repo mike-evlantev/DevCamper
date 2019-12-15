446228e51f09e12386b1952893a7157a272a93dc
Mike Evlantev <mikeev21@gmail.com>
2019-12-15 14:14:42 -0800

    forgot password route

--- routes/auth.js
+++ routes/auth.js
@@ -3,11 +3,13 @@ const { protect } = require("../middleware/auth");
 const {
   registerAsync,
   loginAsync,
-  getMeAsync
+  getMeAsync,
+  forgotPasswordAsync
 } = require("../controllers/auth");
 
 router.route("/register").post(registerAsync);
 router.route("/login").post(loginAsync);
 router.route("/me").get(protect, getMeAsync);
+router.route("/forgotpassword").post(forgotPasswordAsync);
 
 module.exports = router;
