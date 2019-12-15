9ccd99feb5e465f79bbc3189a2dc725014a7636b
Mike Evlantev <mikeev21@gmail.com>
2019-12-14 21:33:30 -0800

    login logic and route

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,3 +19,29 @@ exports.registerAsync = asyncHandler(async (req, res, next) => {
   const token = user.getSignedJwtToken();
   res.status(200).json({ success: true, token });
 });
+
+// @route   POST api/v1/auth/login
+// @desc    Login a user
+// @access  Public
+exports.loginAsync = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+  // Validate email and password
+  if (!email || !password) {
+    return next(
+      new ErrorResponse("Please provider and email and password", 400)
+    );
+  }
+  // Check for user
+  const user = await User.findOne({ email }).select("+password"); // returns user object with password
+  if (!user) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
+  if (!isMatch) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
+  // Create token
+  const token = user.getSignedJwtToken();
+  res.status(200).json({ success: true, token });
+});
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
-const { registerAsync } = require("../controllers/auth");
+const { registerAsync, loginAsync } = require("../controllers/auth");
 
 router.route("/register").post(registerAsync);
+router.route("/login").post(loginAsync);
 
 module.exports = router;
