720dcae073f844baa26b9e6740c1783469fbf611
Mike Evlantev <mikeev21@gmail.com>
2019-12-15 18:08:24 -0800

    added link to docs

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,3 +1,4 @@
+// https://github.com/sendgrid/sendgrid-nodejs/tree/master/packages/mail
 const sendEmail = require("@sendgrid/mail");
 sendEmail.setApiKey(process.env.SENDGRID_API_KEY);
 module.exports = sendEmail;
