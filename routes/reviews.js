9da5276827b6056c0a3509cb5de9cc47958128ee
Mike Evlantev <mikeev21@gmail.com>
2019-12-16 21:08:28 -0800

    create review routing

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,20 +1,24 @@
 const router = require("express").Router({ mergeParams: true }); // mergeParams: Preserve the req.params values from the parent router.
 const {
   getReviewsAsync,
-  getReviewByIdAsync
+  getReviewByIdAsync,
+  createReviewAsync
 } = require("../controllers/reviews");
 const collate = require("../middleware/collate");
 const Review = require("../models/Review");
 const { protect, authorize } = require("../middleware/auth");
 
-router.route("/").get(
-  collate(Review, {
-    path: "bootcamp",
-    select: "name description"
-  }),
-  getReviewsAsync
-);
-//   .post(protect, authorize("publisher", "admin"), createCourseAsync);
+router
+  .route("/")
+  .get(
+    collate(Review, {
+      path: "bootcamp",
+      select: "name description"
+    }),
+    getReviewsAsync
+  )
+  .post(protect, authorize("user", "admin"), createReviewAsync);
+
 router.route("/:id").get(getReviewByIdAsync);
 // .put(protect, authorize("publisher", "admin"), updateCourseByIdAsync)
 // .delete(protect, authorize("publisher", "admin"), deleteCourseByIdAsync);
